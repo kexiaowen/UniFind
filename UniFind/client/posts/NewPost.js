13492604cdf34b07209f188a7359d8cdf4fa1b01
Ke Xiaowen <Xiaowen@Kes-MacBook-Pro.local>
2017-06-11 19:16:41 +0800

    fixed bugs in quickform

--- UniFind/client/posts/NewPost.js
+++ UniFind/client/posts/NewPost.js
@@ -1,14 +1,18 @@
 Template.NewPost.events({
   'click .fa-close' : function () {
     Session.set('newPost', false);
+    Session.set('newPostLost', false);
+    Session.set('newPostFound', false);
   },
 
   'click .new-post-lost' : () =>{
     Session.set('newPostLost', true);
+    Session.set('newPostFound', false);
   },
 
   'click .new-post-found' : () =>{
     Session.set('newPostFound', true);
+    Session.set('newPostLost', false);
   },
 
 });
--- UniFind/collections/PostCollections.js
+++ UniFind/collections/PostCollections.js
@@ -1,4 +1,4 @@
-import PostSchema from 'schema.js';
+// import PostSchema from 'schema.js';
 import SimpleSchema from 'simpl-schema';
 SimpleSchema.extendOptions(['autoform']);
 
@@ -20,6 +20,72 @@ PostsLost.allow({
   },
   update: function(userId, doc){
     return !!userId;
+  }
+});
+
+PostSchema = new SimpleSchema ({
+
+  summary: {
+    type: String,
+    label: "Summary"
+  },
+
+  category: {
+    type: String,
+    label: "Category",
+    allowedValues: ["Handphone", "Laptop", "Wallet", "Jacket", "Thumbdrive", "Waterbottle", "Others"],
+    autoform: {
+          afFieldInput: {
+            firstOption: "(Select a category)"
+          }
+    }
+  },
+
+  colour:{
+    type: String,
+    label: "Colour",
+    optional: true,
+    autoform:{
+      type: "select-radio-inline",
+      options: [
+        {label: "White", value: "White"},
+        {label: "Black", value: "Black"},
+        {label: "Gold", value: "Gold"},
+        {label: "Silver", value: "Silver"},
+        {label:  "Others", value: "Others"}
+      ]
+    }
+
+  },
+  desc: {
+    type: String,
+    label: "Description (Useful information)",
+    autoform:{
+      type: "textarea",
+      rows: 6
+    }
+  },
+
+  author: {
+    type: String,
+    label: "Author",
+    autoValue: function(){
+      return this.userId
+    },
+    autoform: {
+      type: "hidden"
+    }
+  },
+  createdAt: {
+    type: Date,
+    label: "Created At",
+    autoValue: function(){
+      return new Date()
+
+    },
+    autoform: {
+      type: "hidden"
+    }
   }
 });
 
